f87505979f1714cf28b19a3ad143ad0ee589f8dc
Sama-Liyanage <samadhinilakshana1999@gmail.com>
2023-08-21 12:32:29 +0530

    Update NavBar Component

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from "react-icons/ai";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { BsPersonLinesFill } from "react-icons/bs";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () =>{
+    setNav(true)
+  }
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
@@ -24,12 +32,76 @@ const Navbar = () => {
               <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
             </Link>
           </ul>
+
+          <div onClick={handleNav} className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Image src="/../public/assets/logo.png" width="87" height="35" />
+              <div onClick={handleNav} className="rounded-full shadow-lg  shadow-gray-400 p-3 cursor-pointer">
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4 ">
+                Let's build something legendary together
+              </p>
+            </div>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <li className="py-4 test-sm">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="py-4 test-sm">
+                <Link href="/about">About</Link>
+              </li>
+              <li className="py-4 test-sm">
+                <Link href="/skills">Skills</Link>
+              </li>
+              <li className="py-4 test-sm">
+                <Link href="/projects">Projects</Link>
+              </li>
+              <li className="py-4 test-sm">
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+            <div className="pt-20">
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Let's Connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaLinkedin />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaGithub />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <AiOutlineMail />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsPersonLinesFill />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

a9750678fbdf62ed2dcb995ceef509b1c964bf90
Sama-Liyanage <samadhinilakshana1999@gmail.com>
2023-09-16 09:09:56 +0530

    Complete About Section

--- pages/components/About.jsx
+++ pages/components/About.jsx
@@ -5,9 +5,9 @@ const About = () => {
     <div className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-0">
         <div className="col-span-2">
-          <p className="uppercase text-xl tracking-widest text-">About</p>
-          <h2>Who I am</h2>
-          <p>
+          <p className="uppercase text-xl tracking-widest text-[#5e51e5]">About</p>
+          <h2 className="py-4">Who I am</h2>
+          <p className="py-2 text-gray-600">
             I'm an aspiring frontend developer eagerly setting off on a coding
             journey. Although I'm at the beginning of my adventure, I tackle
             each challenge with boundless enthusiasm and a deep thirst for
@@ -18,10 +18,10 @@ const About = () => {
             path. I'm thrilled to be on this journey of growth and discovery in
             the dynamic world of technology!
           </p>
-          <p>Check out more of my latest projects.</p>
+          <p className="py-2 text-gray-600 underline cursor-pointer">Check out more of my latest projects.</p>
         </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzF8fGxhcHRvcCUyMGZyb250ZW5kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" />
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <img className="rounded-xl" src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzF8fGxhcHRvcCUyMGZyb250ZW5kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" />
         </div>
       </div>
     </div>
